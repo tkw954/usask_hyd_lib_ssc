819da1898cdab4fd3b877ed62b2e7addb9bd29f9
tkw954 <t.wiens@usask.ca>
2015-11-03 16:05:02 -0600

    Bug fix: slblocks.m library reference and rotation properties on ideal volume

--- segmented_pipeline_lib/slblocks.m
+++ segmented_pipeline_lib/slblocks.m
@@ -1,6 +1,6 @@
 function blkStruct = slblocks
   % Specify that the product should appear in the library browser
   % and be cached in its repository
-  Browser.Library = 'pipeline';
+  Browser.Library = 'pipeline_lib';
   Browser.Name    = 'USask Segmented Pipeline';
   blkStruct.Browser = Browser;
