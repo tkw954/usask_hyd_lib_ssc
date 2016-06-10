d451b8e282004541d78a2e53e6093b8856848491
tkw954 <t.wiens@usask.ca>
2016-06-10 11:13:39 -0600

    Adds slblocks

--- slblocks.m
+++ slblocks.m
@@ -2,5 +2,5 @@ function blkStruct = slblocks
   % Specify that the product should appear in the library browser
   % and be cached in its repository
   Browser.Library = 'usask_hyd_lib';
-  Browser.Name    = 'USask Hydraulic Library';
+  Browser.Name    = 'USask Hydraulic Library Simscape';
   blkStruct.Browser = Browser;
